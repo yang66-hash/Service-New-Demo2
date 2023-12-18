e8f070e8aff8396363767049f040df6341da66f5
yang66-hash <2019023491@qq.com>
2023-12-18 15:35:24 +0800

    add post request in TravelController.java and add annotation in deploy.yaml for routeservice

--- TravelService/src/main/java/com/yang/controller/TravelController.java
+++ TravelService/src/main/java/com/yang/controller/TravelController.java
@@ -16,45 +16,35 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TravelController extends MObject {
 
-
-
     @Autowired
     private RestTemplate restTemplate;
-
-
-
     /**
      * @param
      * @return get the detail info of the traveller by useId
-     * call the interface in Route Service to get the route info of the traveller
+     * invoke the interface in Route Service to get the route info of the traveller
      */
     @ResponseBody
     @PostMapping("/getDetailInfo")
     @MRestApiType
-    public MResponse getDetailInfo(@RequestParam("userId") String userId, @RequestHeader HttpHeaders httpHeaders){
+    public MResponse getDetailInfo(@RequestParam("userId") String userId
+                                 , @RequestHeader HttpHeaders httpHeaders){
         MResponse result = new MResponse();
-
         if (userId.equals("00001")){
-
             MultiValueMap<String,Object> multiValueMap = new LinkedMultiValueMap<>();
             multiValueMap.add("userId",userId);
 
             HttpEntity httpEntity = new HttpEntity(multiValueMap,httpHeaders);
-            ResponseEntity<MResponse> responseResponseEntity = restTemplate.exchange("http://routeservice/getRouteInfo", HttpMethod.POST,httpEntity,MResponse.class);
-
-//            MResponse mResponse1 = restTemplate.postForObject(url,multiValueMap,MResponse.class);
+            ResponseEntity<MResponse> responseResponseEntity =
+                    restTemplate.exchange("http://routeservice/getRouteInfo",
+                            HttpMethod.POST,httpEntity,MResponse.class);
             if (responseResponseEntity.getBody().getStatus().equals("Success")){
                 result.setValueMap(responseResponseEntity.getBody().getValueMap());
             }
         }
         result.set("name","张三");
         result.set("type","单人游");
-
         return result;
-
-
     }
-
     /**
      * @param
      * @return search the seat info of the traveller by flight
@@ -62,13 +52,12 @@ public class TravelController extends MObject {
     @ResponseBody
     @PostMapping("/getSeatDistribute")
     @MRestApiType
-    public MResponse getSeatDistribute(@RequestParam(value = "flight") String flight, @RequestHeader HttpHeaders httpHeaders){
-
+    public MResponse getSeatDistribute(@RequestParam(value = "flight") String flight,
+                                       @RequestHeader HttpHeaders httpHeaders){
         MResponse result = new MResponse();
         if (flight.equals("MU5542")){
             result.set("seat", "06B");
         }
         return  result;
     }
-
 }
